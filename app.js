89eec611671cd3e8b312c44a3534265d2f012d4f
AngheloM <macha.anghelo@gmail.com>
2017-11-22 14:17:21 -1000

    if statements

---
+++ app.js
@@ -0,0 +1,14 @@
+var greenalien = 5500;
+
+var redalien = 2500;
+
+var bluealien = 2000;
+
+function alieninvasion (green,red,blue){
+  if (green > 5000) {
+    output = "MOTHER OF GOD, not the green ones"
+  }
+
+  if (green + red > 8000 ) {
+    output = "DOOOOOM"
+  }
