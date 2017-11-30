f1dba91f316c4c4bba989e3f9257a890a44ea4a3
AngheloM <macha.anghelo@gmail.com>
2017-11-29 14:17:53 -1000

    finsihed

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var greenalien = 5500;
 
-var redalien = 2500;
+var redalien = 3000;
 
-var bluealien = 2000;
+var bluealien = 1500;
 
 function alieninvasion (green,red,blue){
-  if (green > 5000) {
-    output = "MOTHER OF GOD, not the green ones"
+  if ((green + red + blue) < 10000) {
+    console.log()
   }
 
-  if (green + red > 8000 ) {
-    output = "DOOOOOM"
+  if (green >= 5000) {
+    console.log("MOTHER OF GOD, not the green ones");
   }
+
+  if ((green + red) > 8000 ) {
+    console.log("DOOOOOM");
+  } 
+
+  if ((red + blue) < 3000) {
+    console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+  }
+}
+  alieninvasion(greenalien,redalien,bluealien)
